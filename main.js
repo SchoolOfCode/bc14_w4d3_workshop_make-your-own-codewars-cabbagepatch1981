7bb4e70a789547f29630e8c772955c18a3fffda2
Victoria Burton <vlaburton@gmail.com>
2023-04-07 15:15:15 +0100

    basic kata written

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-/* 
-👉 Write your kata here!
-*/
+/* You are doing a fast-paced web development bootcamp. For each thing you understand (well done!) there are five things you don’t.  Once you get to one hundred things you don’t understand return ‘OK, it's time to ask Steve - but don't worry, you're still doing great!’ as a string. Otherwise return ‘Keep going! There is a lot you’re not going to understand - but you're doing great!’ */
 
-//👉 Write the function your CodeWarriors will start with below here:
+//👉 Write your kata here! 🍏
+
+function askSteve(understood) {
+  let notUnderstood = understood * 5;
+
+  if (notUnderstood >= 100) {
+    return "OK, it's time to ask Steve - but don't worry, you're still doing great!";
+  } else {
+    return "Keep going! There is a lot you're not going to understand - but you're doing great!";
+  }
+}
+console.log(askSteve(20));
+
+//👉 Write the function your CodeWarriors will start with below here: 🍏
+function askSteve(understood) {
+  // Your code here
+  return;
+}
